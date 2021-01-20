58dafc61599fe0218cf936e1c76bb4b88f2d2032
nevesade <adenasiocarvalho19@gmail.com>
2021-01-20 11:51:58 +0000

    Add orders list static

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -2,29 +2,38 @@
 
 import React from 'react';
 
-import { StyleSheet, Text, View } from 'react-native';
-import Header  from '../Header';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import Header from '../Header';
+import OrderCard from '../OrderCard'
 
 function Orders() {
 
- 
+
 
     return (
 
         <>
 
-        <Header/>
-            <View >
+            <Header />
+            <ScrollView  style={styles.container}>
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
 
-                <Text>Listagem de pedidos</Text>
 
-               
-            </View>
+            </ScrollView>
 
         </>
     );
 }
 const styles = StyleSheet.create({
+
+    container: {
+        paddingRight: '5%',
+        paddingLeft: '5%'
+    },
 
 
 
