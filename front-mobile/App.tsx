84b8e6b2c031b0a29f81e6bb2545e4179a9193b5
nevesade <adenasiocarvalho19@gmail.com>
2021-01-20 11:22:20 +0000

    Add home page

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import  Header  from './src/Header';
+import Home from './src/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header/>
+    
       <StatusBar style="auto" />
+      <Header/>
+      <Home/>
     </View>
   );
 }
