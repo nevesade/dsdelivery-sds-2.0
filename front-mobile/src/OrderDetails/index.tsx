1684cdeb0d378cdff673e2f93233c06bccb6e560
nevesade <adenasiocarvalho19@gmail.com>
2021-01-20 20:32:08 +0000

    Add styles on OrderDetails

--- front-mobile/src/OrderDetails/index.tsx
+++ front-mobile/src/OrderDetails/index.tsx
@@ -4,8 +4,9 @@ import { useNavigation } from '@react-navigation/native';
 import AppLoading from 'expo-app-loading';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { RectButton, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import  Header  from '../Header';
+import OrderCard from '../OrderCard';
 import { Order } from '../types';
 
 
@@ -34,8 +35,17 @@ function OrderDetails({ route } : Props) {
 
         <>
             <Header />
-            <View >
-                <Text>Detalhes do pedido {order.id}</Text>
+            <View  style={styles.container}>
+                <OrderCard order={order} />
+                <RectButton style={styles.button}>
+                    <Text style={styles.buttonText}>INICIAR NAVEGAÇÃO</Text>
+                </RectButton>
+                <RectButton style={styles.button}>
+                    <Text style={styles.buttonText}>CONFIRMAR ENTREGA</Text>
+                </RectButton>
+                <RectButton style={styles.button}>
+                    <Text style={styles.buttonText}>CANCELAR </Text>
+                </RectButton>
 
             </View>
 
@@ -46,6 +56,35 @@ function OrderDetails({ route } : Props) {
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create(
+
+    {
+        container: {
+          paddingRight: '5%',
+          paddingLeft: '5%'
+        },
+        button: {
+          backgroundColor: '#DA5C5C',
+          flexDirection: 'row',
+          borderRadius: 10,
+          marginTop: 40,
+          alignItems: 'center',
+          justifyContent: 'center'
+        },
+        buttonText: {
+          paddingTop: 15,
+          paddingBottom: 15,
+          paddingLeft: 50,
+          paddingRight: 50,
+          fontWeight: 'bold',
+          fontSize: 18,
+          color: '#FFF',
+          letterSpacing: -0.24,
+          fontFamily: 'OpenSans_700Bold'
+        }
+      }
+
+
+);
 
 export default OrderDetails;
